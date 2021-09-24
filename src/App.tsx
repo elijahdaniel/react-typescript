22967ba1cf9068c86e8170e8125b521e3bb61735
elijahdaniel <e.paminsan@gmail.com>
2021-09-23 19:05:20 -0700

    interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Message message='This is a simple message' />
-      </header>
-    </div>
-  );
+class App extends Component<any> {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <Message name='Elijah' message='This is a simple message' />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
