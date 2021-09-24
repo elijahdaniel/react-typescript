d97b6185553c6e41b89449d047d91ea1969a82dc
elijahdaniel <e.paminsan@gmail.com>
2021-09-23 18:46:36 -0700

    basic types and complex types

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // ----- basic types -----
+  // let firstValue: string = 'Manny';
+  // let firstValue: number = 22;
+  // let firstValue: boolean = false;
+  // let firstValue: number[] = [2, 4, 23];
+  // let firstValue: Array<string> = ['2', '4', 'jake'];
+
+  // ----- tuple -----
+  let aTuple: [string, number] = ['john', 123];
+
+  // ----- enum -----
+  // enum Codes {first, second} // initialize later
+  enum Codes {
+    first = 1,
+    second = 2,
+  }
+
+  // ----- any -----
+  let firstName: any = 'Elijah';
+
+  // ----- void -----
+  const warning = (): void => {
+    console.log('Warning');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* The value {firstValue} is of {typeof firstValue} type! */}
+          {/* The value {aTuple[0]} is of {typeof aTuple[0]} type! */}
+          The value {firstName} is of {typeof firstName} type!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
